7e70f8b65d34ece675f0c2cf12ad2f0dada3fc6c
Micky Hulse <mickyhulse@gmail.com>
2013-09-14 01:59:40 -0700

    Bumped version to 1.0.0.
    Need to git tag 1.0.0 and I can close the last 0.x.x issue!!!!!
    WOOT!!!!!!! (#94)

--- demo/demo.js
+++ demo/demo.js
@@ -7,7 +7,7 @@
  * @docs https://github.com/mhulse/rex
  * @copyright Copyright (c) 2013 Micky Hulse.
  * @license Released under the Apache License, Version 2.0.
- * @version 0.4.0
+ * @version 1.0.0
  * @date 2013/09/14
  */
 
